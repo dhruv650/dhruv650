055b0e07480d2b2ad77a42670bba8edd53ff43ea
dhruv650 <136941190+dhruv650@users.noreply.github.com>
2024-09-02 05:15:45 +0530

    Update
    This is setup of choco.

---
+++ setup.sh
@@ -0,0 +1,10 @@
+ps
+Set-ExecutionPolicy Bypass -Scope Process -Force
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco --version
+choco install nmap -y
+
+
+@powershell -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+choco install metasploit
