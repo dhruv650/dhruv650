9c0cfa4ec7fd08575c1d5bfdd4c61873f30d577b
dhruv650 <136941190+dhruv650@users.noreply.github.com>
2024-09-02 05:16:23 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,3 +8,5 @@ choco install nmap -y
 @powershell -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 choco install metasploit
+
+msfvenom
